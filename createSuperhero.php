cbac8101327a937f28d66c444b7a1b1f8663002a
Arnaud-J <arnaud.jabin@gmail.com>
2015-10-26 14:54:08 +0000

    superhero form (lab 2) - corrections

--- createSuperhero.php
+++ createSuperhero.php
@@ -11,18 +11,15 @@ $forename = $_POST["forename"] ? $_POST["forename"] : $default;
 $surname = $_POST["surname"] ? $_POST["surname"] : $default;
 $dob = $_POST["dob"] ? $_POST["dob"] : $default;
 $gender = $_POST["gender"] ? $_POST["gender"] : $default;
-$fly = $_POST["fly"] ? $_POST["fly"] : false;
-$lasers = $_POST["lasers"] ? $_POST["lasers"] : false;
-$superstrength = $_POST["superstrength"] ? $_POST["superstrength"] : false;
+$powers = $_POST["powers"] ? $_POST["powers"] : null;
+
 
 echo "<h1>" . $forename . " " . $surname . "</h1>
         <p>"
             . $dob . "<br/>"
             . $gender . "<br/>"
             . "Superpowers: "
-            . ($fly ? "Fly ; " : "")
-            . ($lasers ? "Lasers ; " : "")
-            . ($superstrength ? "Super-strength ; " : "")
+            . $powers
         . "</p>" ;
 ?>
 </body>
--- formSuperhero.php
+++ formSuperhero.php
@@ -9,19 +9,19 @@
     Register new Superhero
 </h1>
 <form action="createSuperhero.php" method="post">
-    <label>Forename</label><input type="text" name="forename"/>
-    <label>Surname</label><input type="text" name="surname"/>
-    <label>Date of birth</label><input type="date" name="dob">
+    <label>Forename</label><input type="text" name="forename"/><br/>
+    <label>Surname</label><input type="text" name="surname"/><br/>
+    <label>Date of birth</label><input type="date" name="dob"><br/>
     <label>Gender</label>
     <select name="gender">
         <option value="">Gender</option>
         <option value="Female">Female</option>
         <option value="Male">Male</option>
-    </select>
-    <label>Superpowers</label>
-    <input type="checkbox" name="fly" content="Fly">
-    <input type="checkbox" name="lasers" content="Lasers">
-    <input type="checkbox" name="superstrength" content="Super-strength">
+    </select><br/><br/>
+    <label>Superpowers</label><br/>
+    <input type="checkbox" name="powers" value="fly">Fly<br/>
+    <input type="checkbox" name="powers" value="lasers">Lasers<br/>
+    <input type="checkbox" name="powers" value="superstrength">Super-strength<br/>
     <input type="submit" value="Submit">
 </form>
 </body>
