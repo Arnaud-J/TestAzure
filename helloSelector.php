9ce7cef8b678ed9d1691fa6f4c488c5c6e1b0ba2
Arnaud-J <arnaud.jabin@gmail.com>
2015-11-02 14:45:09 +0000

    hello printer update

--- helloSelector.php
+++ helloSelector.php
@@ -9,6 +9,10 @@
     <a href="./helloPrinter.php?planet=Earth">Earth</a>
     <a href="./helloPrinter.php?planet=Mars">Mars</a>
     <a href="./helloPrinter.php?planet=Uranus">Uranus</a>
+    <form action="./helloPrinter.php" method="get">
+        <input type="text" name="planet" placeholder="Planet"/>
+        <input type="submit" value="Submit">
+    </form>
 </p>
 </body>
 </html>
