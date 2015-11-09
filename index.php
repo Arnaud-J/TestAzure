db3b1e6372f60c4120d6c6969d28ec9c7fde7150
Arnaud-J <arnaud.jabin@gmail.com>
2015-11-09 14:00:59 +0000

    lab6 setup - DB + DataAccess

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting('-1');
 
 require_once "commonFunctions.php";
-require_once "";
+require_once "DataAccess.php";
 
 $sectionID = isset($_GET['sectionID']) ? $_GET['sectionID'] : "1";
 $title = DataAccess::getTitle($sectionID);
