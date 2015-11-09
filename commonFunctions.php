e1192e2f658911f6b5a58016c33bd2e3b5db724c
Arnaud-J <arnaud.jabin@gmail.com>
2015-11-09 16:06:43 +0000

    lab6 setup - DB + DataAccess

--- commonFunctions.php
+++ commonFunctions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "Topic.php";
+
 function isEmpty($var) {
     return isset($var) && $var == "";
 }
@@ -25,4 +27,31 @@ function htmlFoot() {
             </body>
         </html>
     ';
+
+    function htmlTopics($topics) {
+        echo '
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    Topics
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">';
+        foreach($topics as $topic) {
+            echo'
+                <li><a href="#">'
+                .$topic
+                .'</a></li>
+            ';
+        }
+        echo '
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        ';
+    }
 }
--- index.php
+++ index.php
@@ -22,5 +22,22 @@ foreach($topics as $topic) {
 echo $section;
 
 
+
 htmlFoot();
 ?>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            Topics
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
