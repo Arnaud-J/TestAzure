1c936d2daf33838492e315e0a8f2fad84f55fe76
Arnaud-J <arnaud.jabin@gmail.com>
2015-11-09 16:09:18 +0000

    lab6 setup - DB + DataAccess

--- commonFunctions.php
+++ commonFunctions.php
@@ -25,9 +25,10 @@ function htmlFoot() {
             </body>
         </html>
     ';
+}
 
-    function htmlTopics($topics) {
-        echo '
+function htmlTopics($topics) {
+    echo '
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -38,18 +39,17 @@ function htmlFoot() {
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">';
-        foreach($topics as $topic) {
-            echo'
+    foreach($topics as $topic) {
+        echo'
                 <li><a href="#">'
-                .$topic
-                .'</a></li>
+            .$topic
+            .'</a></li>
             ';
-        }
-        echo '
+    }
+    echo '
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
         </nav>
         ';
-    }
 }
--- index.php
+++ index.php
@@ -15,9 +15,7 @@ $topics = DataAccess::getTopics();
 
 htmlHead("Index","","");
 
-foreach($topics as $topic) {
-    echo $topic;
-}
+htmlTopics($topics);
 
 echo $section;
 
@@ -25,19 +23,3 @@ echo $section;
 
 htmlFoot();
 ?>
-
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            Topics
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
