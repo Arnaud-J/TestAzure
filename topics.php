be9fb419984a22233dd9856e4bce6be837c741f6
Arnaud-J <arnaud.jabin@gmail.com>
2015-11-30 16:14:00 +0000

    twitter webserver

--- topics.php
+++ topics.php
@@ -1,9 +1,5 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting('-1');
 
-require_once "commonFunctions.php";
 require_once "Section.php";
 require_once "Topic.php";
 require_once "BD.php";
